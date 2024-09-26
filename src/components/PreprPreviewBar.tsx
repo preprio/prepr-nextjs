170c8cd934f43f2fe4774bb56bb3e19e0b844a28
Kevintjuhz <k.quaedvlieg@outlook.com>
2024-09-26 16:52:42 +0200

    WIP previewbar

--- src/components/PreprPreviewBar.tsx
+++ src/components/PreprPreviewBar.tsx
@@ -24,6 +24,7 @@ export function PreprPreviewBar(props: {
 }) {
     const { activeSegment, activeVariant, data } = props
 
+    const emptyVariant = 'A'
     const emptySegment = {
         body: 'Choose segment',
     }
@@ -34,22 +35,40 @@ export function PreprPreviewBar(props: {
     )
 
     const [selectedVariant, setSelectedVariant] = useState<string | null>(
-        (!activeVariant && 'A') || activeVariant === 'LAST_VERSION' ? 'A' : 'B'
+        activeVariant || 'A'
     )
 
     const router = useRouter()
     const pathname = usePathname()
 
-    const handleUpdateVariant = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        const variant = e.target.value
+    const handleUpdateVariant = (variant: any) => {
         setSelectedVariant(variant)
 
         const params = new URLSearchParams({})
 
-        if (variant !== 'Choose variant') {
-            params.append('a-b-testing', variant as string)
+        params.append('a-b-testing', variant as string)
+
+        router.push(`${pathname}?${params.toString()}`, {
+            scroll: false,
+        })
+        router.refresh()
+    }
+
+    const handleUpdateSegment = (value: any) => {
+        setSelectedSegment(value)
+
+        const segment = value.reference_id
+
+        if (!segment) {
+            return
+        }
+
+        const params = new URLSearchParams({})
+
+        if (segment !== 'Choose segment') {
+            params.append('segments', segment as string)
         } else {
-            params.append('a-b-testing', 'null')
+            params.append('segments', 'null')
         }
 
         router.push(`${pathname}?${params.toString()}`, {
@@ -58,40 +77,23 @@ export function PreprPreviewBar(props: {
         router.refresh()
     }
 
-    // const handleUpdateSegment = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    //     const segment = e.target.value
-    //     setSelectedSegment(segment)
-    //
-    //     const params = new URLSearchParams({})
-    //
-    //     if (segment !== 'Choose segment') {
-    //         params.append('segments', segment as string)
-    //     } else {
-    //         params.append('segments', 'null')
-    //     }
-    //
-    //     router.push(`${pathname}?${params.toString()}`, {
-    //         scroll: false,
-    //     })
-    //     router.refresh()
-    // }
+    const handleReset = () => {
+        setSelectedSegment(emptySegment)
+        setSelectedVariant(emptyVariant)
 
-    // const handleReset = () => {
-    //     setSelectedSegment(null)
-    //     setSelectedVariant(null)
-    //
-    //     const params = new URLSearchParams({})
-    //     params.append('segments', 'null')
-    //     params.append('a-b-testing', 'null')
-    //
-    //     router.push(`${pathname}?${params.toString()}`, {
-    //         scroll: false,
-    //     })
-    // }
+        const params = new URLSearchParams({})
+        params.append('segments', 'null')
+        params.append('a-b-testing', 'null')
+
+        router.push(`${pathname}?${params.toString()}`, {
+            scroll: false,
+        })
+        router.refresh()
+    }
 
     return (
-        <div className="prp-py-4 prp-px-5 md:prp-px-19.5 prp-bg-indigo-default">
-            <div className="prp-flex prp-gap-y-4 prp-flex-wrap prp-justify-between">
+        <div className="prp-py-4 prp-px-5 md:prp-px-19.5 prp-bg-indigo-default prp-sticky prp-top-0 prp-z-[1000] prp-base">
+            <div className="prp-flex prp-gap-y-4 prp-gap-x-6 prp-flex-wrap prp-justify-between">
                 {/* Logo & Text */}
                 <div className="prp-flex prp-gap-8 prp-items-center">
                     <span className="prp-h-full prp-flex prp-items-center">
@@ -102,7 +104,7 @@ export function PreprPreviewBar(props: {
                     </span>
                 </div>
 
-                <div className="prp-flex prp-gap-6 prp-items-center prp-justify-end">
+                <div className="prp-flex prp-w-full md:prp-w-auto prp-gap-4 lg:prp-gap-6 prp-items-center">
                     <div className="prp-flex prp-gap-4">
                         <p className="prp-regular-text prp-text-white 2xl:prp-flex prp-items-center prp-gap-2 prp-hidden">
                             <span className="prp-pb-0.5">
@@ -114,9 +116,9 @@ export function PreprPreviewBar(props: {
                         </p>
                         <Listbox
                             value={selectedSegment.slug}
-                            onChange={setSelectedSegment}
+                            onChange={handleUpdateSegment}
                         >
-                            <ListboxButton className="prp-h-10 prp-flex-initial prp-w-[15rem] prp-rounded-md data-[open]:prp-rounded-b-none data-[open]:prp-border-b-white prp-border prp-border-gray-300 prp-items-center prp-bg-white prp-justify-center prp-px-4 prp-regular-text prp-text-gray-500">
+                            <ListboxButton className="prp-h-10 prp-flex-initial prp-w-[13rem] md:prp-w-[15rem] prp-max-w-[15rem] prp-rounded-md data-[open]:prp-rounded-b-none data-[open]:prp-border-b-white prp-border prp-border-gray-300 prp-items-center prp-bg-white prp-justify-center prp-px-4 prp-regular-text prp-text-gray-500">
                                 <span className="prp-flex prp-items-center prp-justify-between">
                                     <span>{selectedSegment.body}</span>
                                     <span className="prp-text-gray-900">
@@ -152,11 +154,11 @@ export function PreprPreviewBar(props: {
                         <RadioGroup
                             className="prp-rounded-lg prp-p-1 prp-border prp-border-gray-300 prp-bg-white prp-flex prp-gap-1 prp-h-10 prp-items-center"
                             value={selectedVariant}
-                            onChange={setSelectedVariant}
+                            onChange={handleUpdateVariant}
                         >
                             <Radio
                                 value={'A'}
-                                className="prp-py-2 prp-px-3 prp-rounded-md prp-text-gray-900 prp-regular-text
+                                className="prp-py-2 prp-px-3 prp-rounded-md prp-text-gray-900 prp-regular-text data-[checked]:prp-dropshadow
                                     data-[checked]:prp-bg-indigo-600 data-[checked]:prp-text-white prp-h-8 prp-text-center prp-flex prp-items-center hover:prp-cursor-pointer
                                 "
                             >
@@ -167,7 +169,7 @@ export function PreprPreviewBar(props: {
                             </Radio>
                             <Radio
                                 value={'B'}
-                                className="prp-py-2 prp-px-3 prp-rounded-md prp-text-gray-900 prp-regular-text
+                                className="prp-py-2 prp-px-3 prp-rounded-md prp-text-gray-900 prp-regular-text data-[checked]:prp-dropshadow
                                     data-[checked]:prp-bg-indigo-600 data-[checked]:prp-text-white prp-h-8 prp-text-center prp-flex prp-items-center hover:prp-cursor-pointer
                                 "
                             >
@@ -180,10 +182,7 @@ export function PreprPreviewBar(props: {
                     </div>
 
                     <ResetButton
-                        handleClick={() => {
-                            setSelectedSegment(emptySegment)
-                            setSelectedVariant('A')
-                        }}
+                        handleClick={handleReset}
                         enabled={
                             selectedSegment.reference_id ||
                             selectedVariant !== 'A'
