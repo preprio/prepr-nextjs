884ca4ff99f64a53e182fbbc897ee1a1e949baed
Kevintjuhz <k.quaedvlieg@outlook.com>
2024-11-12 13:56:03 +0100

    Build files

---
+++ dist/components.d.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+
+declare function PreprPreviewBar(props: {
+    activeSegment?: string | null;
+    activeVariant?: string | null;
+    data?: any;
+}): React.JSX.Element;
+
+export { PreprPreviewBar };
